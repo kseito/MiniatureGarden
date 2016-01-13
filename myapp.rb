793eafad149a9de75c886cd579960ec6e0d23c76
kazuto-seito <seito.plusr@gmail.com>
2016-01-13 16:33:44 +0900

    add rss read

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
+require 'rss'
 
 get '/' do
 	@title = 'トップ'
 	erb :index
+end
+
+get '/news' do
+
+    @rss = RSS::Parser.parse("http://rss.sapporobeer.jp/rss/sapporo_1.xml")
+    erb :news
 end
